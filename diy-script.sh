c517c3d4ec6b6d4b452f76fc59aab00a1a8cb307
NineBomb <142368987+NineBomb@users.noreply.github.com>
2024-08-23 11:51:00 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -11,16 +11,10 @@ sed -i 's|/bin/login|/bin/login root|g' feeds/packages/utils/ttyd/files/ttyd.con
 
 sed -i 's/LibWrt/NineBomb/g' package/base-files/files/bin/config_generate
 # 移除要替换的包
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/msd_lite
-rm -rf feeds/packages/net/smartdns
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-netgear
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/luci/applications/luci-app-netdata
-rm -rf feeds/luci/applications/luci-app-serverchan
-#添加源
-#sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git;master' feeds.conf.default
+#rm -rf feeds/packages/net/mosdns
+#rm -rf feeds/packages/net/msd_lite
+#rm -rf feeds/packages/net/smartdns
+
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
